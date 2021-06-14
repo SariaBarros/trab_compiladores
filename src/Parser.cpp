36567c3120f28901adbddf3891e67b7942f0c414
Sandy <sandy@localhost.localdomain>
2021-06-14 01:06:17 -0400

    .

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -22,3 +22,241 @@ void Parser::run(){
 
     cout << "Compilação encerrada com sucesso!\n";
 }
+
+void Parser::program(){
+    //não tenho certeza do que fazer com o epsilon
+}
+
+void Parser::classList(){
+    classDecl();
+        classList_Linha();
+}
+
+void Parser::classList_Linha(){
+    //dont know
+}
+
+void Parser::classDecl(){
+    
+}
+
+void Parser::classDecl_Linha(){
+
+}
+
+void Parser::classBody(){
+
+}
+
+void Parser::varDeclListOpt(){
+
+}
+
+void Parser::varDeclList(){
+
+}
+
+void Parser::varDeclList_Linha(){
+
+}
+
+void Parser::varDecl(){
+
+}
+
+void Parser::varDecl_Linha(){
+
+}
+
+void Parser::varDeclOpt(){
+
+}
+
+void Parser::type(){
+
+}
+
+void Parser::constructDeclListOpt(){
+
+}
+
+void Parser::constructDeclList(){
+
+}
+
+void Parser::constructDeclList_Linha(){
+
+}
+
+void Parser::constructDecl(){
+
+}
+
+void Parser::methodDeclListOpt(){
+
+}
+
+void Parser::methodDeclList(){
+
+}
+
+void Parser::methodDeclList_Linha(){
+
+}
+
+void Parser::methodDecl(){
+
+}
+
+void Parser::methodDecl_Linha(){
+
+}
+
+void Parser::methodBody(){
+
+}
+
+void Parser::paramListOpt(){
+
+}
+
+void Parser::paramList(){
+
+}
+
+void Parser::paramList_Linha(){
+
+}
+
+void Parser::param(){
+
+}
+
+void Parser::param_Linha(){
+
+}
+
+void Parser::statementsOpt(){
+
+}
+
+void Parser::statements(){
+
+}
+
+void Parser::statements_Linha(){
+
+}
+
+void Parser::statement(){
+
+}
+
+void Parser::atribStat(){
+
+}
+
+void Parser::atribStat_Linha(){
+
+}
+
+void Parser::printStat(){
+
+}
+
+void Parser::readStat(){
+
+}
+
+void Parser::returnStat(){
+
+}
+
+void Parser::superStat(){
+
+}
+
+void Parser::ifStat(){
+
+}
+
+void Parser::ifStat_Linha(){
+    
+}
+
+void Parser::forStat(){
+
+}
+
+void Parser::atribStatOpt(){
+
+}
+
+void Parser::expressionOpt(){
+
+}
+
+void Parser::lValue(){
+
+}
+
+void Parser::lValue_Linha(){
+
+}
+
+void Parser::lValueComp(){
+
+}
+
+void Parser::lValueComp_Linha(){
+
+}
+
+void Parser::expression(){
+
+}
+
+void Parser::expression_Linha(){
+
+}
+
+void Parser::allocExpression(){
+
+}
+
+void Parser::numExpression(){
+
+}
+
+void Parser::numExpression_Linha(){
+
+}
+
+void Parser::term(){
+
+}
+
+void Parser::term_Linha(){
+
+}
+
+void Parser::unaryExpression(){
+
+}
+
+void Parser::factor(){
+
+}
+
+void Parser::argList(){
+
+}
+
+void Parser::argListOpt(){
+
+}
+
+void Parser::argList_Linha(){
+    
+}
+
--- src/Parser.h
+++ src/Parser.h
@@ -12,36 +12,48 @@ class Parser{
         void program();
 
         void classList();
+        void classList_Linha();
         void classDecl();
+        void classDecl_Linha();
         void classBody();
 
         void varDecl();
+        void varDecl_Linha();
         void varDeclOpt();
         void varDeclList();
+        void varDeclList_Linha();
         void varDeclListOpt();        
 
         void constructDecl();
         void constructDeclListOpt();
         void constructDeclList();
+        void constructDeclList_Linha();
 
         void term();
+        void term_Linha();
         void type();
         void factor();
 
         void methodBody();
         void methodDecl();
+        void methodDecl_Linha();
         void methodDeclList();
+        void methodDeclList_Linha();
         void methodDeclListOpt();
         
         void param();
+        void param_Linha();
         void paramList();
+        void paramList_Linha();
         void paramListOpt();
         
         void statement();
         void statements();
+        void statements_Linha();
         void statementsOpt();
 
         void atribStat();
+        void atribStat_Linha();
         void atribStatOpt();
         
         void printStat();
@@ -49,18 +61,24 @@ class Parser{
         void returnStat();
         void superStat();
         void ifStat();
+        void ifStat_Linha();
         void forStat();
 
         void lValue();
+        void lValue_Linha();
         void lValueComp();
+        void lValueComp_Linha();
 
         void expression();
+        void expression_Linha();
         void expressionOpt();
         void allocExpression();
         void numExpression();
+        void numExpression_Linha();
         void unaryExpression();
         
         void argList();
+        void argList_Linha();
         void argListOpt();
 
 
